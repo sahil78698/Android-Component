6fbf19de9ff1709f9942c033739bb25ab1a071c3
sahil78698 <sahilhossain999coc@gmail.com>
2022-03-26 23:29:14 +0530

    updated

--- app/android/src/main/java/component/support/android/CoreBase.java
+++ app/android/src/main/java/component/support/android/CoreBase.java
@@ -1,21 +1,30 @@
 package component.support.android;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
 import android.widget.Toast;
 
 public class CoreBase {
     public static SharedPreferences sharedPreferences;
     public static SharedPreferences.Editor editor;
 
-    public static void InitFunctions(Context context) {
+    public static void InitFunctions(Context context, String uri) {
         sharedPreferences = context.getSharedPreferences("app-data", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
-        if (!Component.Main_itemRe.equals(Component.Main_itemMainRe) || !Component.Main_itemInRe.equals(Component.Main_itemMainInre)) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Component.Main_itemRe.equals(Component.Main_itemMainRe) || !Component.Main_itemInRe.equals(Component.Main_itemMainInre)) {
+                editor.putBoolean("securityBreach", true);
+                editor.commit();
+                throw new RuntimeException("error");
+            } else {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(uri.trim())));
+            }
+        } else {
             editor.putBoolean("securityBreach", true);
             editor.commit();
-            Toast.makeText(context, "Detected From LM", Toast.LENGTH_SHORT).show();
-            throw new RuntimeException("error");
         }
 
     }
