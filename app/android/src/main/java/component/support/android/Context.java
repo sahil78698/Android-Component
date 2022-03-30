340de0a2efc63c757aff5be9415589b5b1ed2bc3
sahil78698 <sahilhossain999coc@gmail.com>
2022-03-30 17:03:14 +0530

    updated

--- app/android/src/main/java/component/support/android/Context.java
+++ app/android/src/main/java/component/support/android/Context.java
@@ -1,5 +1,6 @@
 package component.support.android;
 
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,44 +12,105 @@ public class Context {
     public static int in2 = 10;
     public static boolean completed = false;
     public static boolean isCompleted = false;
+    public static AppCompatButton download;
+    public static AppCompatButton unlock;
+    public static TextView timer;
+
     public static void reInit(TextView timer) {
+        completed = false;
+        isCompleted = false;
         in = 10;
         in2 = 10;
-        timer.setText("10\nSeconds");
     }
 
+    public static void Init(TextView mTimer, AppCompatButton mUnlock, AppCompatButton mDownload) {
+        download = (AppCompatButton) mDownload;
+        unlock = (AppCompatButton) mUnlock;
+        timer = (TextView) mTimer;
 
-    public static void update(TextView timer, AppCompatButton unlock) {
+    }
+
+    public static void update() {
         in = 10;
-        for (int i = 0; i < 11; i++) {
-            try {
-                Thread.sleep(1000);
-                timer.setText(in + "\nSeconds");
-                in--;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        UpdateMainData updateMainData = new UpdateMainData();
+        updateMainData.execute();
+    }
+
+    public static void updaterMainHeader() {
+        UpdateData updateData = new UpdateData();
+        updateData.execute();
+    }
+
+    public static class UpdateMainData extends AsyncTask<String, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            timer.setText("10\nSeconds");
+            in = 10;
         }
-        unlock.setVisibility(View.VISIBLE);
-        unlock.setEnabled(true);
-        isCompleted = true;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            for (int i = 0; i < 11; i++) {
+                try {
+                    Thread.sleep(1000);
+                    in--;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(in);
+            }
+            return "Updated";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            timer.setText(in + "\nSeconds");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            unlock.setVisibility(View.VISIBLE);
+            unlock.setEnabled(true);
+            isCompleted = true;
+        }
     }
 
 
-    public static void updaterMainHeader(TextView timer, AppCompatButton download) {
-        in2 = 10;
-        for (int i = 0; i < 11; i++) {
-            try {
-                Thread.sleep(1000);
-                timer.setText(in2 + "\nSeconds");
-                download.setText(in2 + " Seconds");
-                in2--;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    public static class UpdateData extends AsyncTask<String, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            timer.setText("10\nSeconds");
+            in2 = 10;
+            download.setText(in2 + " Seconds");
         }
-        download.setBackground(download.getContext().getDrawable(R.drawable.completed));
-        download.setText("Download");
-        completed = true;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            for (int i = 0; i < 11; i++) {
+                try {
+                    Thread.sleep(1000);
+                    in2--;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(in2);
+            }
+            return "Updated";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            timer.setText(in + "\nSeconds");
+            download.setText(in2 + " Seconds");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            download.setBackground(download.getContext().getDrawable(R.drawable.completed));
+            download.setText("Download");
+            completed = true;
+        }
     }
+
+
 }
--- app/android/src/main/java/component/support/android/ContextAware.java
+++ app/android/src/main/java/component/support/android/ContextAware.java
@@ -1,5 +1,6 @@
 package component.support.android;
 
+import android.os.AsyncTask;
 import android.view.View;
 
 import androidx.appcompat.widget.AppCompatButton;
@@ -7,28 +8,53 @@ import androidx.appcompat.widget.AppCompatButton;
 public class ContextAware {
     public static int in = 10;
     public static boolean completed = false;
+    public static AppCompatButton button;
 
-    public static void s(AppCompatButton button) {
-        updater(button);
+    public static void s(AppCompatButton mButton) {
+        button = mButton;
     }
 
-    public static void updater(AppCompatButton button) {
-        button.setEnabled(false);
-        in = 10;
-        for (int i = 0; i < 11; i++) {
-            try {
-                Thread.sleep(1000);
-                button.setText(in + " Seconds");
-                in--;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    public static void updater() {
+        UpdateMainDataMain updateMainDataMain = new UpdateMainDataMain();
+        updateMainDataMain.execute();
+    }
+
+    public static class UpdateMainDataMain extends AsyncTask<String, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            button.setEnabled(false);
+            button.setText("10 Seconds");
+            in = 10;
         }
-        button.setText("Continue");
-        button.setVisibility(View.VISIBLE);
-        button.setBackground(button.getContext().getDrawable(R.drawable.button));
-        button.setEnabled(true);
-        completed = true;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            for (int i = 0; i < 11; i++) {
+                try {
+                    Thread.sleep(1000);
+                    in--;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(in);
+            }
+            return "Updated";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            button.setText(in + " Seconds");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            button.setText("Continue");
+            button.setVisibility(View.VISIBLE);
+            button.setBackground(button.getContext().getDrawable(R.drawable.button));
+            button.setEnabled(true);
+            completed = true;
+        }
     }
+
 
 }
